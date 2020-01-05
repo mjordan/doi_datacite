83cda12f0d4e1512700a7238ada3e2311396394a
Mark Jordan <mjordan@sfu.ca>
2020-01-05 20:16:43 +0000

    Added logic to pass DataCite resource type from form to minter.

--- src/Minter/Dois.php
+++ src/Minter/Dois.php
@@ -32,12 +32,14 @@ class Dois {
    * Mints the identifier.
    *
    * @param object $entity
-   *   The entity.
+   *   The node, etc.
+   * @param mixed $extra
+   *   Extra data the minter needs, for example from the node edit form.
    *
    * @return string
    *   The identifier.
    */
-  public function mint($entity) {
+  public function mint($entity, $extra = NULL) {
     $config = \Drupal::config('doi_datacite.settings');
     $namespace = $config->get('doi_datacite_api_endpoint');
     $namespace = $config->get('doi_datacite_prefix');
@@ -45,6 +47,11 @@ class Dois {
     $namespace = $config->get('doi_datacite_username');
     $namespace = $config->get('doi_datacite_password');
     $namespace = $config->get('doi_datacite_combine_creators');
+    // For $extra coming from node edit form.
+    if (is_object($extra) && method_exists($extra, 'getValue')) {
+      $datacite_resource_types = $extra->getValue('doi_datacite_resource_types_values', []);
+      \Drupal::logger('doi_datacite')->debug(var_export(array_values($datacite_resource_types), true), []);
+    }
     return 'Please stand by.... the DataCite DOI module is still under development.';
   }
 
